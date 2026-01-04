e646838c52f50645fbb1eb332de1ff6ebffc8db4
Filipe Bertolucci <filipe@bertolucci.it>
2026-01-04 02:03:48 +0100

    pwa.last mods

--- vite.config.js
+++ vite.config.js
@@ -5,29 +5,46 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
+
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "logo.png"],
+
+      includeAssets: [
+        "favicon.ico",
+        "logo.png"
+      ],
+
       manifest: {
-        name: "CLCI",
+        name: "Chiesa Luterana Confessionale d'Italia",
         short_name: "CLCI",
+        description: "Sito ufficiale della Chiesa Luterana Confessionale d'Italia",
+
         start_url: "/",
+        scope: "/",
+
         display: "standalone",
+
         background_color: "#000000",
         theme_color: "#000000",
+
         icons: [
           {
             src: "/logo.png",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "/logo.png",
             sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+            type: "image/png"
+          }
+        ]
       },
-    }),
-  ],
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+        navigateFallback: "/index.html"
+      }
+    })
+  ]
 });
